f47cc1594e2132d094156dc5f78e9644bc9f56a6
Dylan McGannon <peltium@gmail.com>
2019-07-13 15:47:53 +1000

    add delete cascale triggers

--- migrations/2019-07-07-035142_create_participant/up.sql
+++ migrations/2019-07-07-035142_create_participant/up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE participant (
   id CHAR(16) DEFAULT random_string(16),
-  board_id CHAR(16) REFERENCES board NOT NULL,
+  board_id CHAR(16) REFERENCES board ON DELETE CASCADE NOT NULL,
   owner BOOLEAN NOT NULL DEFAULT 'f',
   PRIMARY KEY (id, board_id)
 )
